2837f689e60fef8341523dcc78bc382613b27ac6
Anton Smatanik <smatanik.anton@gmail.com>
2026-03-01 22:41:54 +0100

    Small fix

--- app/(shared-layout)/blog/page.tsx
+++ app/(shared-layout)/blog/page.tsx
@@ -4,9 +4,9 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
 import { fetchQuery } from "convex/nextjs";
 import { Metadata } from "next";
+import { cacheLife, cacheTag } from "next/cache";
 import Image from "next/image";
 import Link from "next/link";
-import { connection } from "next/server";
 import { Suspense } from "react";
 
 // export const dynamic = "force-static";
@@ -41,10 +41,10 @@ export default function BlogPage() {
 }
 
 async function LoadBlogList() {
-  // "use cache";
-  // cacheLife("hours");
-  // cacheTag("blog-list");
-  await connection();
+  "use cache";
+  cacheLife("hours");
+  cacheTag("blog-list");
+  // await connection();
   const data = await fetchQuery(api.posts.getPosts);
 
   return (
